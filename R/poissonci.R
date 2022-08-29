6aff8cce39aed9d64359c125b0d04d9692829f54
SigurdJanson <emailjs@web.de>
2022-08-29 19:26:58 +0200

    Added function for poisson confidence intervals

---
+++ R/poissonci.R
@@ -0,0 +1,115 @@
+# Confidence Intervals for Poisson mean
+# Taken from https://github.com/AndriSignorell/DescTools/R/StatsAndCIs.r
+# See https://search.r-project.org/CRAN/refmans/DescTools/html/PoissonCI.html
+
+
+
+
+#' poissonci
+#'
+#' Computes the confidence intervals of a poisson distributed variable's lambda.
+#' Several methods are implemented, see details.
+#'
+#' @param x number of events.
+#' @param n time base for event count.
+#' @param conf.level confidence level, default is 0.95.
+#' @param alternative character string specifying the side of the confidence
+#' interval, must be one of "two.sided" (default), "left" or "right".
+#' @param method character string specifying which method to use; can be one
+#' out of "wald", "score" (default), "exact" or "byar".
+#' @note Tanusit (2012) recommends "Score" intervals for small sample `n`
+#' and "Wald" for large ones.
+#' @return A vector with 3 elements for estimate, lower confidence interval,
+#' and upper for the upper one.
+#' @references Patil & Kulkarni (2012).
+#' ["Comparison of Confidence Intervals for the Poisson Mean: Some New Aspects"]
+#' (https://www.ine.pt/revstat/pdf/rs120203.pdf).
+#' REVSTAT - Statistical Journal, 10(2),  p. 211–227
+#' Tanusit, M. (2012). ["Two-Side Confidence Intervals for the Poisson Means"]
+#' (http://www.ijmo.org/papers/189-S083.pdf). International Journal of Modeling and Optimization,
+#' 2 (5), p. 589-591
+#' @importFrom stats poisson.test qnorm
+poissonci <- function(x, n = 1, conf.level = 0.95,
+                     alternative = c("two.sided", "left", "right"),
+                     method = c("exact", "score", "wald","byar")) {
+
+  if(missing(method)) method <- "exact"
+  if(missing(alternative)) alternative <- "two.sided"
+
+
+  iPoissonCI <- function(x, n = 1, conf.level = 0.95, sides = c("two.sided","left","right"),
+                         method = c("exact","score", "wald","byar")) {
+
+    # see also: pois.conf.int {epitools}
+
+    sides <- match.arg(sides, choices = c("two.sided","left","right"), several.ok = FALSE)
+    if(sides!="two.sided")
+      conf.level <- 1 - 2*(1-conf.level)
+
+    if(missing(method)) method <- "score"
+
+    if(length(conf.level) != 1)  stop("'conf.level' has to be of length 1 (confidence level)")
+    if(conf.level < 0.5 | conf.level > 1)  stop("'conf.level' has to be in [0.5, 1]")
+
+    alpha <- 1 - conf.level
+    z <- qnorm(1-alpha/2)
+
+    lambda <- x/n
+
+    switch( match.arg(arg=method, choices=c("exact","score", "wald", "byar")),
+            "exact" = {
+              ci <- poisson.test(x, n, conf.level = conf.level)$conf.int
+              lwr.ci <- ci[1]
+              upr.ci <- ci[2]
+            },
+            "score" = {
+              term1 <- (x + z^2/2)/n
+              term2 <- z * n^-0.5 * sqrt(x/n + z^2/(4*n))
+              lwr.ci <- term1 - term2
+              upr.ci <- term1 + term2
+            },
+            "wald" = {
+              term2 <- z*sqrt(lambda/n)
+              lwr.ci <- lambda - term2
+              upr.ci <- lambda + term2
+            },
+            "byar" = {
+              xcc <- x + 0.5
+              zz  <- (z/3) * sqrt(1/xcc)
+              lwr.ci <- (xcc * (1 - 1/(9 * xcc) - zz)^3)/n
+              upr.ci <- (xcc * (1 - 1/(9 * xcc) + zz)^3)/n
+            }
+    )
+
+    ci <- c( est=lambda, lwr.ci=lwr.ci, upr.ci=upr.ci )
+
+    if(sides=="left")
+      ci[3] <- Inf
+    else if(sides=="right")
+      ci[2] <- -Inf
+
+    return(ci)
+  }
+
+  # handle vectors
+  # which parameter has the highest dimension
+  lst <- list(x=x, n=n, conf.level=conf.level, sides=alternative, method=method)
+  maxdim <- max(lengths(lst)) #max(unlist(lapply(lst, length)))
+  # recycle all params to maxdim
+  lgp <- lapply(lst, rep, length.out=maxdim)
+
+  res <- sapply(1:maxdim, \(i) iPoissonCI(x=lgp$x[i], n=lgp$n[i],
+                                          conf.level=lgp$conf.level[i],
+                                          sides=lgp$sides[i],
+                                          method=lgp$method[i]))
+  rownames(res)[1] <- c("est")
+
+  lgn <- .recycle(x=if(is.null(names(x))) paste("x", seq_along(x), sep=".") else names(x),
+                  n=if(is.null(names(n))) paste("n", seq_along(n), sep=".") else names(n),
+                  conf.level=conf.level, sides=alternative, method=method)
+  xn <- apply(as.data.frame(lgn[sapply(lgn, \(x) length(unique(x)) != 1)]), 1, paste, collapse=":")
+
+  colnames(res) <- xn
+
+  return(t(res))
+}
