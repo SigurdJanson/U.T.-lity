8c15b17e5e0e777e651f49b4b30928145e3ddd7d
SigurdJanson <emailjs@web.de>
2022-09-04 23:13:21 +0200

    Testing and optimising confidence interval plots

--- R/ciplot.R
+++ R/ciplot.R
@@ -18,24 +18,51 @@ ciplot.default <- function(x, ...) stop("Not able to determine the type of 'x'")
 
 
 
+#' @describeIn ciplot ...
+#' @export
+ciplot.ci <- function(x, ...) stop("not yet implemented") #TODO
+
+
+
+#' @describeIn ciplot Plot the result of a call to [t.test()].
+#' @export
+ciplot.htest <- function(x, ...) {
+  ciplot_default(
+    lower=x$conf.int[1],
+    upper=x$conf.int[2], ...
+  )
+}
+
+
+
+#' @describeIn ciplot Plot the result of a call to [ci.numeric()].
+#' @export
+ciplot.cidf <- function(x, ...) {
+  ciplot_default(
+    lower=x$lower,
+    upper=x$upper,
+    names=rownames(x), ...
+  )
+}
+
+
+
 #' @describeIn ciplot Simplified base version to draw confidence limit plots.
 #'
 #' @param lower numeric vector with the lower limits of the confidence intervals.
 #' @param upper numeric vector with the upper limits of the confidence intervals.
 #' @param names A vector of names applied to each pair of `lower`/`upper`.
-#' @param engine draw the plot either with `ggplot2` or `graphics`.
+#' @param lib draws the plot either with `ggplot2` or `graphics`.
 #' @param ... further arguments to be passed on to other methods
 #'
 #' @return A chart visualising confidence intervals
 #' @export
-ciplot_default <- function(lower, upper, names=NA, engine = c("ggplot", "graphics"), ...) {
-  engine <- match.arg(engine)
-  if (engine == "ggplot" && !require(ggplot2) )
-    engine <- "graphics"
-  else
-    "ggplot2"
+ciplot_default <- function(lower, upper, names=NA, lib = c("ggplot", "graphics"), ...) {
+  lib <- match.arg(lib)
+  if (lib == "ggplot" && !require(ggplot2, quietly = TRUE) )
+    lib <- "graphics"
 
-  if (engine == "ggplot" || missing(engine))
+  if (lib == "ggplot")
     ciplot_gg(lower, upper, names, ...)
   else
     ciplot_gr(lower, upper, names, ...)
@@ -51,8 +78,8 @@ ciplot_gr <- function(lower, upper, names=NA, ...) {
 
   # maximumrows <- 13 # used to set the bar height, ylim = c(0,maximumrows)
 
-  lower <- rev(lower) # reverse order
-  upper <- rev(upper)
+  #lower <- rev(lower) # reverse order
+  #upper <- rev(upper)
   width <- upper - lower
   top <- upper + (width * 0.05) # 1 - upper
   bottom <- lower - (width * 0.05)
@@ -88,7 +115,7 @@ ciplot_gg <- function(lower, upper, names=NA, ...) {
     mean  = rev(upper + lower) / 2,
     names = if (.isAlive(names)) rev(names) else rev(seq_along(lower))
   )
-  print(dt)
+  #---print(dt)
   top <- dt$upper + (dt$width * 0.05) # 1 - upper
   bottom <- dt$lower - (dt$width * 0.05)
 
@@ -103,24 +130,3 @@ ciplot_gg <- function(lower, upper, names=NA, ...) {
 }
 
 
-
-#' @describeIn ciplot Plot the result of a call to [t.test()].
-#' @export
-ciplot.htest <- function(x, ...) {
-  ciplot_default(
-    lower=x$conf.int[1],
-    upper=x$conf.int[2], ...
-  )
-}
-
-
-
-#' @describeIn ciplot Plot the result of a call to [ci.numeric()].
-#' @export
-ciplot.cidf <- function(x, ...) {
-  ciplot_default(
-    lower=x$lower,
-    upper=x$upper,
-    names=rownames(x), ...
-  )
-}
--- tests/testthat/test-ciplot.R
+++ tests/testthat/test-ciplot.R
@@ -1,9 +1,21 @@
 # PLOT CONFIDENCE ===================
 
-test_that("Base CI plot works", {
+test_that("Base CI plot works for lib=graphics", {
   lwr <- seq(0, 10, 10/3)
-  upr <- (seq(0, 10, 10/3)+1)*2
+  upr <- (lwr + 1) * 2
 
   # Assert
-  vdiffr::expect_doppelganger("ciplot", ciplot_default(lwr, upr, names=LETTERS[1:4]))
+  vdiffr::expect_doppelganger("ciplot_gr",
+                              ciplot_default(lwr, upr, names=LETTERS[1:4], lib="graphics"))
 })
+
+
+test_that("Base CI plot works for lib=ggplot", {
+  lwr <- seq(0, 10, 10/3)
+  upr <- (lwr + 1) * 2
+
+  # Assert
+  vdiffr::expect_doppelganger("ciplot_gg",
+                              ciplot_default(lwr, upr, names=LETTERS[1:4], lib="ggplot"))
+})
+
