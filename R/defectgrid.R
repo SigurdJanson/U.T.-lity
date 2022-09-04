0225b9db5d03e79298dac8f7c72b5ef2e4c1dfc3
SigurdJanson <emailjs@web.de>
2022-09-04 22:40:16 +0200

    Testing and fixing defect grid functions

--- R/defectgrid.R
+++ R/defectgrid.R
@@ -2,12 +2,32 @@
 #'
 #' The defect grid is a  class to store a PROBLEM-BY-PARTICIPANT matrix.
 #'
-#' @param x a numeric or logical matrix with subjects in rows and defects
+#' @param x a numeric or logical matrix with participants in rows and defects
 #' in columns.
+#' @details the `defectgrid` object has an attribute `type` that can be either
+#' `exists` or `count`. `exist` indicates that each cell contains only information
+#' about the existence of the defect but not it's frequency (contrary to `count`).
+#'
+#' A second class "U.T.lity" indicates that this object is from this package.
+#'
+#' If `x` is a vector it will be coerced to a matrix with one row (i.e. participant).
 #' @return A defect grid object.
+#' @export
 defectgrid_new <- function(x) {
-  Type <- c("count", "exists")
+  type <- c("count", "exists")
+  if (is.logical(x))
+    type <- type[2L]
+  else if (is.numeric(x)) {
+    if (is.integer(x)  && all(is.element(unique(x), 0:1L)))
+      type <- type[2L]
+    else
+      type <- type[1L]
+  } else {
+    stop("Objects must bei either numeric or logical to become a defect grid")
+  }
+  if (!is.matrix(x)) x <- matrix(x, 1L)
 
+  attr(x, "type") <- type
   class(x) <- c("U.T.lity", "defectgrid", class(x))
   return(x)
 }
--- tests/testthat/test-defectgrid.R
+++ tests/testthat/test-defectgrid.R
@@ -1,7 +1,57 @@
+# defectgrid_new =============
+
+test_that("'defectgrid_new()' creates an object that is still recognised as matrix", {
+  x <- defectgrid_new(matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4))
+  expect_s3_class(x, "matrix")
+})
+
+test_that("'defectgrid_new()' coerces vectors a matrix", {
+  row <- 3L; col <- 4L
+  x <- defectgrid_new(c(1,0,1,0, 0,1,0,1, 0,0,1,1))
+  expect_s3_class(x, "matrix")
+  expect_identical(nrow(x), 1L)
+  expect_identical(ncol(x), row * col)
+})
+
+test_that("'defectgrid_new()' sets correct type attributes", {
+  testobj <- matrix(as.integer(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4))
+  x <- defectgrid_new(testobj)
+  expect_identical(attr(x, "type"), "exists")
+
+  x <- defectgrid_new(matrix(c(1,0,2,0, 0,1,0,1, 0,0,1,1), 3, 4))
+  expect_identical(attr(x, "type"), "count")
+
+  testobj <- matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4)
+  x <- defectgrid_new(as.logical(testobj))
+  expect_identical(attr(x, "type"), "exists")
+})
+
+# is.defectgrid ==============
+test_that("'defectgrid_new()' creates an object detected by 'is.defectgrid()'", {
+  x <- defectgrid_new(matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4))
+  expect_true(is.defectgrid(x))
+})
+
+test_that("'defectgrid_new()' creates an object detected by 'is.defectgrid()'", {
+  x <- matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4)
+  class(x) <- c("U.T.lity", "defectgrid")
+  expect_true(is.defectgrid(x))
+})
+
+
+
+test_that("matrix is not denied by 'is.defectgrid()'", {
+  x <- matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4)
+  expect_false(is.defectgrid(x))
+})
+
+
+# pxpplot_default ===========
 test_that("4 regular columns work", {
 
   # Act
-  result <- pxpplot.matrix( matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4), percentage=TRUE )
+  dg <- defectgrid_new(matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4))
+  result <- pxpplot_default(dg, percentage=TRUE )
 
   # Assert
   expect_identical(
@@ -15,7 +65,8 @@ test_that("4 regular columns work", {
 test_that("...adding 3 columns of darkfigures works", {
 
   # Act
-  result <- pxpplot.matrix( matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4), darkfigure=3, percentage=TRUE )
+  dg <- defectgrid_new(matrix(c(1,0,1,0, 0,1,0,1, 0,0,1,1), 3, 4))
+  result <- pxpplot_default( dg, darkfigure=3, percentage=TRUE )
 
   # Assert
   expect_identical(
