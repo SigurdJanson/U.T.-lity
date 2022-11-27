13f968675b44e2e10ac7d0ba6f27ef46153b69d9
SigurdJanson <emailjs@web.de>
2022-11-27 16:54:17 +0100

    Optimised tests

--- tests/testthat/test-binomci.R
+++ tests/testthat/test-binomci.R
@@ -18,26 +18,27 @@ test_that("sample cases work", {
   expect_equal(result$lower[1], 0.03575611114501953125, tolerance=0.05)
   expect_equal(result$upper[1], 0.50660800933837890625, tolerance=0.05)
 
-  758
   # stattools.crab.org/Calculators/binomialConfidence.htm
   result <- binomci(758, 10003, conf.level=0.80)
-  expect_equal(result$lower[1], 0.0708, tolerance=0.05)
-  expect_equal(result$upper[1], 0.0811, tolerance=0.05)
+  expect_equal(result$lower[1], 0.0725, tolerance=0.001)
+  expect_equal(result$upper[1], 0.0792, tolerance=0.001)
 })
 
 
 
-# use samples takes fomr the `MKinfer` library
+# use samples taken from the `MKinfer` library
 # using https://rdrr.io/snippets/
 # This test could be improved - it is not very precise
-test_that("one-sided intervals are correct", {
+test_that("one-sided intervals are correct for confidence=95%", {
   expected <- data.frame(
     conf.level  = c(rep(0.95, 6L)),
     x           = rep(c(42, 16, 23), 2L),
     n           = rep(c(43, 112, 256), 2L),
     alternative = factor(c(rep("less", 3L), rep("greater", 3L))),
-    lower       = c(rep(0.00, 3L), c(0.902252, 0.09688296, 0.06457696)),
-    upper       = c(c(0.9947946, 0.2056791, 0.1236894), rep(1.00, 3L))
+    lower       = c(rep(0.00, 3L),
+                    c(0.90225195413828107593, 0.096882964853565414765, 0.064576958381973997847)),
+    upper       = c(c(0.99479459430017136423, 0.20567907712816674293, 0.1236893725354294149),
+                    rep(1.00, 3L))
   )
 
   for (testrow in 1:6) {
@@ -49,7 +50,7 @@ test_that("one-sided intervals are correct", {
     # Act
     result <- binomci(x = .x, n = .n, conf.level = .cl, alternative=as.character(.alt))
     # Assert
-    expect_equal(c(result$lower[1], result$upper[1]), .exp, tolerance=0.075)
+    expect_equal(c(result$lower[1], result$upper[1]), .exp, tolerance=0.07234375, label=paste(testrow, " - ", .alt))
   }
 })
 
